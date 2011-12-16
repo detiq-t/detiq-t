bea41ed43aaa9067af9f308499bb2cd4b15f364e
Benoit Averty <benoit.averty@gmail.com>
2011-12-16 13:43:58 +0000

    Tentative de correction d'un bug (et transfert)

--- ImageIn/BmpImage.cpp
+++ ImageIn/BmpImage.cpp
@@ -6,3 +6,28 @@ BmpImage::BmpImage(std::string filename) : ImageFile(filename)
 {
     //TODO
 }
+
+unsigned int BmpImage::readWidth()
+{
+	return 0;
+}
+
+unsigned int BmpImage::readNbChannels()
+{
+	return 0;
+}
+
+unsigned int BmpImage::readDepth()
+{
+	return 0;
+}
+
+char* BmpImage::readData()
+{
+	return NULL;
+}
+
+void BmpImage::writeData(char* data, unsigned int width, unsigned int height, unsigned int nChannels, unsigned int depth)
+{
+
+}
