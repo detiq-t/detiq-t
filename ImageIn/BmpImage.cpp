fc7fac164387df93b3e69635d63f517c27299003
Florian Teyssier <florian.teyssier@insa-rennes.fr>
2012-02-26 17:18:41 +0000

    Commentaires BmpImage

--- ImageIn/BmpImage.cpp
+++ ImageIn/BmpImage.cpp
@@ -5,30 +5,52 @@ using namespace imagein;
 BmpImage::BmpImage(std::string filename)
  : ImageFile(filename)
 {
+	/* Setting off the EasyBMP library warnings that are useless for the user of the ImageIn library
+	  (replace with SetEasyBMPwarningsOn() in case you want them to appear */
 	SetEasyBMPwarningsOff();
+	// We create a new BMP object (from EasyBMP library)
 	workImg = new BMP();
+	/* We try to read from the file at the given adress (filename),
+	   if the file exists, its content is loaded, if not we try to create
+	   a new file, if it fails an exception is sent */
     if(!workImg->ReadFromFile(_filename.c_str())) {
-		workImg->WriteToFile(_filename.c_str());
-        /*std::string msg = "The file ";
-        msg += _filename;
-        msg += " could not be opened.";
-        throw ImageFileException(msg, __LINE__, __FILE__);*/
+		if(!workImg->WriteToFile(_filename.c_str())) {
+			std::string msg = "The file ";
+			msg += _filename;
+			msg += " could not be opened or created.";
+			throw ImageFileException(msg, __LINE__, __FILE__);
+		}
     }
 }
 
 char* BmpImage::readData()
 {
+	// We create a new BMP object (from EasyBMP library)
+	if(workImg != NULL) delete workImg;
+	workImg = new BMP();
+	/* We try to read from the file at the given adress (filename),
+	   if the file exists, its content is loaded, otherwise an exception is sent */
+    if(!workImg->ReadFromFile(_filename.c_str())) {
+		std::string msg = "The file ";
+        msg += _filename;
+        msg += " could not be opened.";
+        throw ImageFileException(msg, __LINE__, __FILE__);
+    }
+	// We get the image's parameters and create a char matrix with them
 	unsigned int w=readWidth(), h=readHeight(), c=readNbChannels();
 	char* data = new char[w * h * c];
+	// We run through the BMP object's matrix of pixels and load each channel into our char matrix
 	unsigned int i, j;
 	RGBApixel px;
 	for(i=0;i<w;i++) {
 		for(j=0;j<h;j++) {
+			// Getting the current pixel
 			px = workImg->GetPixel(i,j);
+			// Every pixel is in a RGBA form, we will always get the RGB components, and the Alpha component when necessary
 			data[c*(i+j*w)] = (char)px.Red;
 			data[c*(i+j*w)+1] = (char)px.Green;
 			data[c*(i+j*w)+2] = (char)px.Blue;
-			if(c==4) data[c*(i+j*w)+3] = (char)px.Alpha;
+			if(c==4) data[c*(i+j*w)+3] = (char)px.Alpha; // In case there are 4 channels, we get the Alpha channel too
 		}
 	}
 	return data;
@@ -36,20 +58,33 @@ char* BmpImage::readData()
 
 void BmpImage::writeData(const char* const data, unsigned int width, unsigned int height, unsigned int nChannels, unsigned int depth)
 {
-	delete workImg;
+	// We create a new BMP object (from EasyBMP library)
+	if(workImg != NULL) delete workImg;
 	workImg = new BMP();
+	// We initiate the BMP object size
 	workImg->SetSize(width,height);
+	// We run through our char matrix and load each channel into the BMP object's matrix of pixels
 	unsigned int i, j;
 	RGBApixel* px;
 	for(i=0;i<width;i++) {
 		for(j=0;j<height;j++) {
+			// Getting the current pixel
 			px=(*workImg)(i,j);
+			// Every pixel is in a RGBA form, we will always load the RGB components, and the Alpha component when necessary
 			px->Red = data[nChannels*(i+j*width)];
 			px->Green = data[nChannels*(i+j*width)+1];
 			px->Blue = data[nChannels*(i+j*width)+2];
-			if(nChannels == 4) px->Alpha = data[nChannels*(i+j*width)+3];
-			//TODO gérer les profondeurs supérieures au char
+			if(nChannels == 4) px->Alpha = data[nChannels*(i+j*width)+3]; // In case there are 4 channels, we load the Alpha channel too
+			//TODO handling of a depth other than char
 		}
 	}
-    workImg->WriteToFile(_filename.c_str());
+    /* We try to write to the file at the given adress (filename),
+	   if the file doesn't exist, it is created, otherwise it is overwritten
+	   if the writing fails an exception is sent */
+    if(!workImg->WriteToFile(_filename.c_str())) {
+		std::string msg = "The file ";
+        msg += _filename;
+        msg += " could not be written.";
+        throw ImageFileException(msg, __LINE__, __FILE__);
+    }
 }
--- ImageIn/BmpImage.h
+++ ImageIn/BmpImage.h
@@ -3,6 +3,7 @@
 
 #include "ImageFile.h"
 
+// ImageIn uses the EasyBMP library (http://easybmp.sourceforge.net/), the files from this library are directly included in the project
 #include "EasyBMP.h"
 
 namespace imagein
@@ -22,7 +23,7 @@ namespace imagein
             void writeData(const char* const data, unsigned int width, unsigned int height, unsigned int nChannels, unsigned int depth);
 
         private:
-            BMP* workImg;
+            BMP* workImg; // The EasyBMP library object that will be used to read and write in a BMP file
     };
 	
 	inline unsigned int BmpImage::readHeight()
@@ -37,12 +38,17 @@ namespace imagein
 
 	inline unsigned int BmpImage::readNbChannels()
 	{
+		/* There are always 3 channels (RGB), whatever the depth is,
+		   except when the depth is 32, then there is a fourth channel (Alpha) */
 		if(readDepth()==32) return 4;
 		else return 3;
 	}
 
 	inline unsigned int BmpImage::readDepth()
 	{
+		/* Since a channel is always represented with 8 bits in ImageIn and there are 
+		  at least 3 channels (RGB), the minimum depth will be 24 bits. When there is
+		  a fourth channel (Alpha), the depth will be 32 bits */
 		if(workImg->TellBitDepth() == 32) return 32;
 		else return 24;
 	}
