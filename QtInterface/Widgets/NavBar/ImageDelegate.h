1a68757fba742e9f63509cacfad698b823a52d15
Samuel Babin <samuel.babin1@insa-rennes.fr>
2012-03-06 17:44:16 +0000

    Ajout de ImageDelegate.cpp

--- QtInterface/Widgets/NavBar/ImageDelegate.h
+++ QtInterface/Widgets/NavBar/ImageDelegate.h
@@ -9,22 +9,22 @@ namespace genericinterface
 {
   class ImageDelegate : public QItemDelegate
   {
-   void paint(QPainter *painter, const QStyleOptionViewItem &option,
-                              const QModelIndex &index) const
-    {
-      QRect rect = option.rect;
-      rect.adjust(5, 5, -5, -5);
-      
-      if (option.state & QStyle::State_Selected)
-        painter->fillRect(option.rect, option.palette.highlight());
-
-      painter->drawImage(rect/*QRectF(30, index.row()*100, 50, 50)*/, QImage(index.data().toString()));
-    }
-
-    QSize sizeHint (const QStyleOptionViewItem & option, const QModelIndex & index) const
-    {
-      return QSize(75,75);
-    }
+    /**
+	 * @brief Redefinition of paint() from QItemDelegate to change de display in the navigation bar.
+	 //TODO parameters
+	 * @param painter xxx
+	 * @param option xxx
+	 * @param index xxx
+	 */
+    void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+    
+    /**
+	 * @brief Redefinition of sizeHint) from QItemDelegate.
+	 //TODO parameters
+	 * @param option xxx
+	 * @param index xxx
+	 */
+    QSize sizeHint(const QStyleOptionViewItem & option, const QModelIndex & index) const;
   };
 }
 
