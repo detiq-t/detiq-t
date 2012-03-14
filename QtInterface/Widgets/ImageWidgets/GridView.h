f4acc145acd9891af0eabfbf07442ff97a3000c5
Thomas Letan <thomas.letan@insa-rennes.fr>
2012-03-14 16:42:47 +0000

    réorganisation dossier Widgets

--- QtInterface/Widgets/ImageWidgets/GridView.h
+++ QtInterface/Widgets/ImageWidgets/GridView.h
@@ -1,32 +1,27 @@
-#ifndef GRIDVIEW_H
-#define GRIDVIEW_H
-
-#include "GraphicsView.h"
-#include "AlternativeImageView.h"
+#ifndef WIDGET_ZOOMWIDGET_H
+#define WIDGET_ZOOMWIDGET_H
 
 #include <Image.h>
-using namespace imagein;
+#include "../ImageWidgets/AlternativeImageView.h"
+
+class QHBoxLayout;
 
 namespace genericinterface
 {
-    class GridView : public AlternativeImageView
-    {
-		Q_OBJECT
-	private:
-		GraphicsView* _view;
-        
-	public:
-		GridView(Image* image);
-		GraphicsView* getGraphicsView();
-		
-	public slots:
-        //void ctrlPressed();
-        
-	signals:
-		void pixelClickedLeft(int x, int y);
-		void pixelClickedRight(int x, int y);
-		void pixelHovered(int x, int y);
-    };
+  class ImageViewer;
+
+  class GridView : public AlternativeImageView
+  {
+  public:
+    GridView(imagein::Image* im, const QString & path, int dx, int dy);
+
+  public slots:
+    void newDisplay(int x, int y);
+
+  private:
+    QHBoxLayout* _layout;
+    ImageViewer* _viewer;
+  };
 }
 
-#endif // GRIDVIEW_H
+#endif
--- QtInterface/Widgets/ImageWidgets/GridWindow.h
+++ QtInterface/Widgets/ImageWidgets/GridWindow.h
@@ -1,49 +1,20 @@
-#ifndef GRIDWINDOW_H
-#define GRIDWINDOW_H
+#ifndef WIDGET_GRIDWINDOW_H
+#define WIDGET_GRIDWINDOW_H
 
-#include <QWidget>
-#include <QLabel>
-#include <QVBoxLayout>
-#include <QScrollArea>
-#include <QFont>
-
-#include <sstream>
-
-#include "ImageWindow.h"
-#include "GridView.h"
-
-#include <Image.h>
-using namespace imagein;
+#include "../ImageWidgets/ImageWindow.h"
 
 namespace genericinterface
 {
-    class GridWindow : public ImageWindow
-    {
-		Q_OBJECT
-    private:
-        GridView* _view;
-		QLabel* _lImageName;
-		QLabel* _lHoveredPixelInfo;
-		QLabel* _lSelectedPixel1Info;
-		QLabel* _lSelectedPixel2Info;
-		QLabel* _lHoveredPixelPosition;
-		QLabel* _lHoveredPixelColor;
-		QLabel* _lSelectedPixel1Position;
-		QLabel* _lSelectedPixel2Position;
-		QLabel* _lSelectedPixel1Color;
-		QLabel* _lSelectedPixel2Color;
-		
-		void init();
-        void initStatusBar();
-        
-    public slots:
-		void showHoveredPixel(int x, int y);
-        void showLeftClickedPixel(int x, int y);
-        void showRightClickedPixel(int x, int y);
-        
-    public:
-		GridWindow(Image* image, ImageWindow* source);
-    };
+  class GridView;
+
+  class GridWindow : public ImageWindow
+  {
+  public:
+    GridWindow(const QString & path, ImageWindow* source = 0, imagein::Rectangle* rect = 0);
+
+  protected:
+    GridView* _view;
+  };
 }
 
-#endif // GRIDWINDOW_H
+#endif
