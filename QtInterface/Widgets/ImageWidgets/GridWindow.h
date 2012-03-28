e5feed7aeca7b72acde8555f5688b1d357d654be
Thomas Letan <thomas.letan@insa-rennes.fr>
2012-03-28 16:08:28 +0000

    doxygen update for missing widgets

--- QtInterface/Widgets/ImageWidgets/GridWindow.h
+++ QtInterface/Widgets/ImageWidgets/GridWindow.h
@@ -3,17 +3,35 @@
 
 #include "../ImageWidgets/ImageWindow.h"
 
+/**
+* @brief Vizualisation of pixels' grid
+*/
+
 namespace genericinterface
 {
   class GridView;
 
+  /**
+  * @brief This widget displays the image with an important zoom, so each pixel can be
+  * seen quickly.
+  *
+  * It helps the user to understand what a pixel is: a simple square of color.
+  * The values of channels are displaying too, directly on the square
+  */
   class GridWindow : public ImageWindow
   {
   public:
+    /**
+    * @brief The constructor of the class
+    *
+    * @param path the path of the image's file
+    * @param source the widget which asked for the creation of the grid
+    * @param rect the original view
+    */
     GridWindow(const QString & path, ImageWindow* source = 0, imagein::Rectangle* rect = 0);
 
   protected:
-    GridView* _view;
+    GridView* _view; /*!< the widget which deals with the pixels display */
   };
 }
 
