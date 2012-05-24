c2363534061aa2b494f13976af7c2e6f53a801c2
Thomas Letan <thomas.letan@insa-rennes.fr>
2012-05-24 13:15:05 +0000

    keeping proportion of images in nav bar

--- GenericInterface/Widgets/NavBar/ImageDelegate.cpp
+++ GenericInterface/Widgets/NavBar/ImageDelegate.cpp
@@ -1,16 +1,35 @@
 #include "ImageDelegate.h"
 
+#include <iostream>
+
 using namespace genericinterface;
 
 void ImageDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
   QRect rect = option.rect;
-  rect.adjust(5, 5, -5, -5);
+  rect.adjust(3, 3, -3, -3);
   
   if (option.state & QStyle::State_Selected)
     painter->fillRect(option.rect, option.palette.highlight());
-	 
-  painter->drawImage(rect/*QRectF(30, index.row()*100, 50, 50)*/, QImage(index.data().toString()));
+
+  QImage img(index.data().toString());
+
+  if (img.width() > img.height())
+  {
+    double scale = (double)rect.width() / (double)img.width();
+    int size = (int) ((double)img.height() * scale);
+    int adjust = - size / 2;
+    rect.adjust(0, adjust, 0, -adjust);
+  }
+  else if (img.height() > img.width()) 
+  {
+    double scale = (double)rect.height() / (double)img.height();
+    int size = (int) ((double)img.width() * scale);
+    int adjust = size / 2;
+    rect.adjust(adjust, 0, -adjust, 0);
+  }
+
+  painter->drawImage(rect/*QRectF(30, index.row()*100, 50, 50)*/, img);
 }
 
 QSize ImageDelegate::sizeHint(const QStyleOptionViewItem & option, const QModelIndex & index) const
