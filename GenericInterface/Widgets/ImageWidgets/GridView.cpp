eb265755e248439db2ca2f9a8da8390d461f8c87
Thomas Letan <thomas.letan@insa-rennes.fr>
2012-05-26 17:43:41 +0000

    modification of pixel grid

--- GenericInterface/Widgets/ImageWidgets/GridView.cpp
+++ GenericInterface/Widgets/ImageWidgets/GridView.cpp
@@ -1,5 +1,6 @@
 #include <QHBoxLayout>
 #include <QGraphicsView>
+#include <vector>
 
 #include "GridView.h"
 #include "ImageViewer.h"
@@ -8,21 +9,73 @@
 using namespace imagein;
 using namespace genericinterface;
 
-GridView::GridView(Image* im, const QString & path, int dx, int dy) : AlternativeImageView(im)
+ChannelSelector::ChannelSelector(const QString& name, int nb)
+  : QRadioButton(name), _nb(nb)
+{
+  if(nb == 0)
+    setChecked(true);
+  QObject::connect(this, SIGNAL(clicked(bool)), this, SLOT(send(bool)));
+}
+
+void ChannelSelector::send(bool b)
+{
+  if(b)
+    emit activated(_nb);
+}
+
+RadioPanel::RadioPanel(int nb)
+{
+  std::vector<QString> names;
+
+  names.resize(nb);
+
+  if(nb == 3)
+  {
+    names[0] = "R";
+    names[1] = "G";
+    names[2] = "B";
+  }
+
+  QHBoxLayout* l = new QHBoxLayout();
+
+  for(int i = 0; i < nb; i++)
+  {
+    ChannelSelector* b = new ChannelSelector(names[i], i);
+    QObject::connect(b, SIGNAL(activated(int)), this, SLOT(rcvActivated(int)));
+    l->addWidget(b);
+  }
+
+  setLayout(l);
+}
+
+void RadioPanel::rcvActivated(int a)
+{
+  emit activated(a);
+}
+
+GridView::GridView(Image* im, const QString & path, int dx, int dy)
+  : AlternativeImageView(im)
 {
     _layout = new QHBoxLayout;
     _viewer = new ImageViewer(path, dx, dy);
 
+    QVBoxLayout *layout = new QVBoxLayout();
 
     QGraphicsView* view = new QGraphicsView;
     view->setScene(_viewer);
 
-    _layout->addWidget(view);
+    RadioPanel* panel = new RadioPanel(im->getNbChannels());
+
+    layout->addWidget(view);
+    layout->addWidget(panel);
+    _layout->addLayout(layout);
 
     ZoomViewer* zv = new ZoomViewer(im, ImageViewer::RECT_W, ImageViewer::RECT_H);
     QGraphicsView* viewz = new QGraphicsView;
     viewz->setScene(zv);
     _layout->addWidget(viewz);
+
+    QObject::connect(panel, SIGNAL(activated(int)), zv, SLOT(channelChosen(int)));
 
     QObject::connect(_viewer, SIGNAL(positionUpdated(int, int)), zv, SLOT(draw(int, int)));
 
--- GenericInterface/Widgets/ImageWidgets/GridView.h
+++ GenericInterface/Widgets/ImageWidgets/GridView.h
@@ -1,17 +1,46 @@
 #ifndef WIDGET_ZOOMWIDGET_H
 #define WIDGET_ZOOMWIDGET_H
 
+#include <QRadioButton>
+
 #include <Image.h>
 #include "../ImageWidgets/AlternativeImageView.h"
 
 class QHBoxLayout;
 
+
 namespace genericinterface
 {
+  class ChannelSelector : public QRadioButton
+  {
+  Q_OBJECT
+  public:
+      ChannelSelector(const QString& name, int nb);
+  signals:
+      void activated(int);
+  public slots:
+      void send(bool);
+  private:
+      int _nb;
+  };
+
+  class RadioPanel : public QWidget
+  {
+  Q_OBJECT
+  public:
+      RadioPanel(int nb = 1);
+  signals:
+      void activated(int);
+
+  private slots:
+      void rcvActivated(int);
+  };
+
   class ImageViewer;
 
   /**
-  * @brief The grid view manages the interaction of the users with the grid pixel. It deals with the events of the mouse and
+  * @brief The grid view manages the interaction of the users with the grid
+  * pixel. It deals with the events of the mouse and
   * the keyboard
   */
   class GridView : public AlternativeImageView
