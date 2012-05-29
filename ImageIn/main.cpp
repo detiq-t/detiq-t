70f529ce865708174077e6c0e193332f72b67be5
Benoit Averty <benoit.averty@gmail.com>
2012-05-29 17:27:40 +0000

    Algorithme de dithering (propagation d'erreur de floyd-steinberg) temps passé : 2h30

--- ImageIn/main.cpp
+++ ImageIn/main.cpp
@@ -1,51 +1,18 @@
 #include <list>
 #include "Image.h"
+#include "Converter.h"
 #include "Algorithm/Otsu.h"
-#include "Algorithm/MorphoMat.h"
+#include "Algorithm/Dithering.h"
 
 using namespace imagein;
 using namespace imagein::algorithm;
 using namespace std;
 
-/*
-int main()
-{
-	for(list<int>::const_iterator it = l.begin() ; it != l.end() ; ++it) {
-		cout << *it << " ";
-	}
-	cout << endl;
-}*/
-int main(int argc, char** argv)
-{}
-
-/*
 int main(int argc, char** argv)
 {
+    Image img("samples/test_dithering.png");
 
+    Dithering d;
 
-	Image_t<depth8_t> img("samples/lena.jpg");
-
-    bool elem[] = {
-        false, true, false,
-        true,  true, true,
-        false, true, false
-    };
-
-    MorphoMat::StructElem<depth8_t> structElem(GrayscaleImage_t<bool>(3, 3, elem), 1, 1);
-    MorphoMat::Gradient<depth8_t> grad(structElem);
-    Image *res = grad(&img);
-    res->save("morpho.jpg");
-
-    return 0;
+    d(Converter<GrayscaleImage>::convert(img))->save("samples/result_dithering.png");
 }
-
-#ifdef AUTREMAIN
-int main(int argc, char* argv[])
-{
-	GrayscaleImage img("samples/test.png");
-	
-	Image* im_binaire = o(&im);
-	im_binaire->save("samples/test.jpg");
-	delete im_binaire;
-}
-*/
